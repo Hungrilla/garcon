d6b2b5fef06fd93c7e051bac4632954d3abdaf2c
Umayr Shahid <umayrr@hotmail.co.uk>
2015-05-03 16:13:04 +0500

    minor changes after addition of helpers

--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,23 @@
-var express       = require('express');
-var route         = express.Router();
+var express = require('express');
+var route = express.Router();
+var routeHelper = require('../helpers/route');
 
 module.exports = controller;
 
-function controller (app){
+function controller(app) {
   // # Routes
   route.get('/users/:id', getUser);
   route.get('/users', getUsers);
 
+  routeHelper.register(app, route);
+
   // # Methods
-  function getUser (req, res){
+  function getUser(req, res) {
     res.json(req.params.id);
   }
 
-  function getUsers (req, res){
-    res.json(['one', 'two']);
+  function getUsers(req, res) {
+    res.json(['Wayne', 'Wilson']);
   }
+
 }
