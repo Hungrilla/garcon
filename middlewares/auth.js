7de170501f40561b5aea2e832f548a1c675bcce5
Mohammad Umair Khan <mohammad.umair.k@gmail.com>
2015-05-11 00:52:49 +0500

    auth integrated with client facebook auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var authHelper = require('../helpers/auth');
+var request = require('request');
 
 module.exports = {
-  authIsValid: authIsValid
+  authIsValid: authIsValid,
+  fbAuthValid: fbAuthValid
 };
 
 /**
@@ -21,9 +23,28 @@ function authIsValid(req, res, next){
   }
   else{
     req.user = {
-      email: claims.email,
+      userId: claims.subject,
       thirdPartyAccessToken: claims.accessToken
     };
     next();
   }
+}
+
+/**
+ * Middleware - verifies Facebook accessToken
+ * @method
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {function} next - Callback for the next function.
+ */
+function fbAuthValid(req, res, next){
+  var url = 'https://graph.facebook.com/me?access_token=' + req.body.accessToken;
+  request(url, function (error, response) {
+    if (!error && response.statusCode == 200) {
+      next();
+    }
+    else{
+      res.status(401).send();
+    }
+  });
 }
